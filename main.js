e8554eb0e3c54ac0f00b165c346d8d8bbccc94c2
Alexandre Beaudouin <alexandre.beaudouin@dekra.com>
2020-08-18 14:45:24 +0200

    refacto Session to class

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { Init } = require('./services/axiosInit');
 Init();
 module.exports.Search = require('./lib/search');
 module.exports.Item = require('./lib/item').Item;
-module.exports.Session = require('./lib/session').Session;
+module.exports.Session = require('./lib/session');
 module.exports.User = require('./lib/user').User;
 module.exports.utils = require('./lib/utils');
 module.exports.FILTERS = require('./lib/filters');
--- services/axiosInit.js
+++ services/axiosInit.js
@@ -1,19 +1,30 @@
 /* eslint-disable no-undef */
 const axios = require('axios').default;
-const axiosCookieJarSupport = require('axios-cookiejar-support').default;
-const tough = require('tough-cookie');
-axiosCookieJarSupport(axios);
+const Session = require('../lib/session');
 
-const cookieJar = new tough.CookieJar();
 
 function Init() {
   axios.interceptors.request.use(
     (config) => {
-      cookieJar.getCookies(config.url, function (err, cookies) {
-        config.headers.cookie = cookies.join('; ');
-      });
-      console.log('config', config);
+      if(config.session === null || !config.session){
+        const newSession = new Session(config);
+        console.log(config.session)
+        config.session = newSession;
+        config.headers = newSession.getHeader();
+        config.headers = {...config.headers, cookie: config.session.cookieJar}
+      }
+      console.log(config)
       return config;
+    },
+    (error) => {
+      return Promise.reject(`HTTP err : ${error}`);
+    }
+  );
+
+  axios.interceptors.response.use(
+    (response) => {
+      console.log('res', response)
+      return response
     },
     (error) => {
       return Promise.reject(`HTTP err : ${error}`);
